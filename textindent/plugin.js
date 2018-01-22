8422ac80bfa429423c134c841a59701e2f89f1fa
Teteruga <34138467+Teteruga@users.noreply.github.com>
2018-01-22 16:02:36 -0200

    Add files via upload

---
+++ textindent/plugin.js
@@ -0,0 +1,69 @@
+CKEDITOR.plugins.add( 'textindent', {
+	icons: 'textindent',
+    availableLangs: {'pt-br':1, 'en':1},
+    lang: 'pt-br, en',
+	init: function( editor ) {
+
+        if(editor.ui.addButton){
+
+            editor.ui.addButton( 'textindent', {
+                label: editor.lang.textindent.labelName,
+                command: 'insert',
+                toolbar: 'insert',
+            });
+        }
+
+        editor.on( 'selectionChange', function()
+            {
+                var style_textindente = new CKEDITOR.style({
+                        element: 'p',
+                        styles: { 'text-indent': '50px' },
+                        overrides: [{
+                            element: 'text-indent', attributes: { 'size': '0'}
+                        }]
+                    });
+
+                if( style_textindente.checkActive(editor.elementPath(), editor) )
+                   editor.getCommand('insert').setState(CKEDITOR.TRISTATE_ON);
+                else
+                   editor.getCommand('insert').setState(CKEDITOR.TRISTATE_OFF);
+                
+        })
+
+        editor.addCommand("insert", {
+            allowedContent: 'p{text-indent}',
+            requiredContent: 'p',
+            exec: function() {
+
+                var style_textindente = new CKEDITOR.style({
+                        element: 'p',
+                        styles: { 'text-indent': '50px' },
+                        overrides: [{
+                            element: 'text-indent', attributes: { 'size': '0'}
+                        }]
+                    });
+
+                var style_no_textindente = new CKEDITOR.style({
+                        element: 'p',
+                        styles: { 'text-indent': '0' },
+                        overrides: [{
+                            element: 'text-indent', attributes: { 'size': '50px' }
+                        }]
+                    });
+
+                if( style_textindente.checkActive(editor.elementPath(), editor) ){
+                    editor.fire('saveSnapshot');
+                    editor.applyStyle(style_no_textindente);
+                    editor.fire('saveSnapshot');
+                }
+                else{
+                    editor.fire('saveSnapshot');
+                    editor.applyStyle(style_textindente);
+                    editor.fire('saveSnapshot');
+                }
+
+            }
+        });
+	}
+
+});
