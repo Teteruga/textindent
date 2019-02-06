a84b6c72a6c0b6c93e32558c57cab95ecce9f8a2
Teteruga <34138467+Teteruga@users.noreply.github.com>
2019-02-06 16:14:46 -0200

    Update plugin.js, added key bind
    All the code of plugin.js was changed to solve a problem where the style text-indent removed all other styles in the tag <p>
    Changed the name of the command to ident-paragraph, was insert.
    Add key bind to activate the indentation, the key Tab is the default, and the ability to change the key or disable this functionality using the variable in config indentationKey

--- textindent/plugin.js
+++ textindent/plugin.js
@@ -1,69 +1,96 @@
 CKEDITOR.plugins.add( 'textindent', {
-	icons: 'textindent',
+    icons: 'textindent',
     availableLangs: {'pt-br':1, 'en':1},
     lang: 'pt-br, en',
 	init: function( editor ) {
 
         var indentation = editor.config.indentation;
+        var indentationKey = editor.config.indentationKey;
 
         if(typeof(indentation) == 'undefined')
-            indentation = '50';
+            indentation = '50px';
+        if(typeof(indentationKey) == 'undefined')
+            indentationKey = 'tab';
 
         if(editor.ui.addButton){
 
             editor.ui.addButton( 'textindent', {
                 label: editor.lang.textindent.labelName,
-                command: 'insert',
-                toolbar: 'insert',
+                command: 'ident-paragraph',
+                toolbar: 'ident-paragraph',
             });
         }
 
+        if( indentationKey !== false){
+
+            editor.on('key', function(ev) {
+                if(ev.data.domEvent.$.key.toLowerCase() === indentationKey.toLowerCase().trim() || ev.data.keyCode === indentationKey){
+                    editor.execCommand('ident-paragraph');
+                    ev.cancel();  
+                }
+            });
+        }
+        
         editor.on( 'selectionChange', function()
             {
                 var style_textindente = new CKEDITOR.style({
                         element: 'p',
-                        styles: { 'text-indent': indentation+'px' },
-                        overrides: [{
-                            element: 'text-indent', attributes: { 'size': indentation+'0'}
-                        }]
-                    });
-
-                if( style_textindente.checkActive(editor.elementPath(), editor) )
-                   editor.getCommand('insert').setState(CKEDITOR.TRISTATE_ON);
-                else
-                   editor.getCommand('insert').setState(CKEDITOR.TRISTATE_OFF);
-
-        })
-
-        editor.addCommand("insert", {
-            allowedContent: 'p{text-indent}',
-            requiredContent: 'p',
-            exec: function() {
-
-                var style_textindente = new CKEDITOR.style({
-                        element: 'p',
-                        styles: { 'text-indent': indentation+'px' },
+                        styles: { 'text-indent': indentation },
                         overrides: [{
                             element: 'text-indent', attributes: { 'size': '0'}
                         }]
                     });
 
-                var style_no_textindente = new CKEDITOR.style({
-                        element: 'p',
-                        styles: { 'text-indent': '0' },
-                        overrides: [{
-                            element: 'text-indent', attributes: { 'size': indentation+'px' }
-                        }]
-                    });
+                if( style_textindente.checkActive(editor.elementPath(), editor) )
+                   editor.getCommand('ident-paragraph').setState(CKEDITOR.TRISTATE_ON);
+                else
+                   editor.getCommand('ident-paragraph').setState(CKEDITOR.TRISTATE_OFF);
+                
+        });
 
-                if( style_textindente.checkActive(editor.elementPath(), editor) ){
-                    editor.fire('saveSnapshot');
-                    editor.applyStyle(style_no_textindente);
+        editor.addCommand("ident-paragraph", {
+            allowedContent: 'p{text-indent}',
+            requiredContent: 'p',
+            exec: function(evt) {
+
+                var range = editor.getSelection().getRanges()[0]; 
+
+                var walker = new CKEDITOR.dom.walker( range ),
+                node;
+
+                while ( ( node = walker.next() ) ) {
+                    if ( node.type == CKEDITOR.NODE_ELEMENT ) {
+                        if(node.getName() === "p"){
+                                editor.fire('saveSnapshot');
+                                
+                                if( node.getStyle('text-indent') ){
+                                    node.removeStyle("text-indent");
+                                    editor.getCommand('ident-paragraph').setState(CKEDITOR.TRISTATE_OFF);
+                                }
+                                else{
+                                    node.setStyle( "text-indent", indentation );
+                                    editor.getCommand('ident-paragraph').setState(CKEDITOR.TRISTATE_ON);
+                                }
+                        }
+                    }
                 }
-                else{
+
+                if(node === null){
+                    
+                    node = editor.getSelection().getStartElement().getAscendant('p', true);
+    
                     editor.fire('saveSnapshot');
-                    editor.applyStyle(style_textindente);
+
+                    if( node.getStyle('text-indent') ){
+                        node.removeStyle("text-indent");
+                        editor.getCommand('ident-paragraph').setState(CKEDITOR.TRISTATE_OFF);
+                    }
+                    else{
+                        node.setStyle( "text-indent", indentation );
+                        editor.getCommand('ident-paragraph').setState(CKEDITOR.TRISTATE_ON);
+                    }
                 }
+                
 
             }
         });
